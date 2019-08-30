ef3394d103dab4c0137b28cf046a3f2bad7a9cfc
Sascha Koch <sascha.koch@web.de>
2019-08-30 13:34:24 +0200

    some things removed to have sth to implement

--- src/main/kotlin/de/smartsquare/hangman/Hangman.kt
+++ src/main/kotlin/de/smartsquare/hangman/Hangman.kt
@@ -15,19 +15,7 @@ fun getStrLn(): IO<String> = IO { readLine() ?: throw IOException("Failed to rea
 
 object Hangman {
 
-    data class State(val name: String, val guesses: Set<Char> = emptySet(), val word: String) {
-
-        val failures: Int = (guesses.toSet().minus(word.toSet())).size
-
-        val playerLost: Boolean = failures > 8
-
-        val playerWon: Boolean = (word.toSet().minus(guesses)).isEmpty()
-    }
-
-    val stateLens: Lens<State, Set<Char>> = Lens(
-            get = { state -> state.guesses},
-            set = { state, value -> state.copy(guesses = value)}
-    )
+    data class State(val player: String)
 
     val dictionary: List<String> by lazy {
         javaClass.classLoader.getResource("words.txt")
@@ -38,65 +26,28 @@ object Hangman {
     }
 
     val hangman: IO<Unit> = fx {
-        putStrLn("Welcome to purely functional hangman").bind()
-        val (name) = getName
-        putStrLn("Welcome $name. Let's begin!").bind()
-        val (word) = chooseWord
-        val state = State(name, word = word)
-        renderState(state).bind()
-        gameLoop(state).bind()
+        // at least: choose a new random word from the dictionary and start the game 'loop' , e.g.
+        putStrLn("Let's try it together.").bind()
+        val (playerName) = getPlayerName
+        val initialState = State(playerName)
+        gameLoop(initialState).bind()
         Unit
     }.fix()
 
-    fun gameLoop(state: State): IO<State> = fx {
-        val guess = getChoice().bind()
-        val updatedState = stateLens.lift { it.plus(guess) }(state)
-        renderState(updatedState).bind()
-        val loop = when {
-            updatedState.playerWon -> putStrLn("Congratulations ${state.name} you won the game").map { false }
-            updatedState.playerLost -> putStrLn("Sorry ${state.name} you lost the game. The word was ${state.word}").map { false }
-            updatedState.word.contains(guess) -> putStrLn("You guessed correctly!").map { true }
-            else -> putStrLn("That's wrong, but keep trying").map { true }
-        }.bind()
-        if (loop) gameLoop(updatedState).bind() else updatedState
-    }.fix()
-
-    val getName: IO<String> = fx {
-        putStrLn("What is your name: ").bind()
+    val getPlayerName: IO<String> = fx {
+        putStrLn("Please enter your name: ").bind()
         val (name) = getStrLn()
         name
     }.fix()
 
-    fun getChoice(): IO<Char> = fx {
-        putStrLn("Please enter a letter").bind()
-        val line = getStrLn().bind()
-        val char = line.toLowerCase().trim().firstOption().fold(
-                {
-                    putStrLn("You did not enter a character")
-                            .flatMap { getChoice() }
-                },
-                {
-                    IO.just(it)
-                }
-        ).bind()
-        char
-    }.fix()
-
-    fun nextInt(max: Int): IO<Int> = IO { Random.nextInt(max) }
-
-    val chooseWord: IO<String> = fx {
-        val rand = nextInt(dictionary.size).bind()
-        dictionary[rand].liftIO().bind()
-    }.fix()
-
     fun renderState(state: State): IO<Unit> {
-        val word = state.word.toList().map { if (state.guesses.contains(it)) " $it " else "   " }
-                .joinToString("")
-        val line = state.word.map { " - " }.joinToString("")
-        val guesses = "Guesses: ${state.guesses.toList().sorted().joinToString("")}"
-        val text = "$word\n$line\n\n$guesses\n"
-        return putStrLn(text)
+        return putStrLn(state.player)
     }
+
+    fun gameLoop(state: State): IO<State> = fx {
+        renderState(state).bind()
+        state
+    }.fix()
 }
 
 fun main() {
